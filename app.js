dc35002f65de01e4bf34195c42b293d616e544b1
Salem Hilal <bichiliad@gmail.com>
2012-09-03 16:16:35 -0400

    Map generation

--- app.js
+++ app.js
@@ -97,6 +97,35 @@ Player.prototype.money = 0;
 Player.prototype.kills = 0; // A parameter that records how many kills you have
 Player.prototype.charm = 10
 
-function run() {
+
+/* Generates a walkable map for the game
+ * Map is of type [{height: int}]
+ *   walkHeight: how high/low falling can occur
+ *	 width: how wide the map should be.
+ */
+function generateMap (walkHeight, width, initialHeight){
+	if(initialHeight == undefined)
+		initialHeight == 200
+
+	function generateChunk(curWidth, prevHeight){
+		var newHeight = prevHeight ((2*Math.random() - 1) * walkHeight)
+		if(curWidth <= 5){
+			var r = [];
+			for(var i=0; i<curWidth; i++){
+				r.push({height: newHeight});
+			}
+			return r; 
+		}
+		else{
+			var chunkWidth = (Math.random() * 5 + 1);
+			var r = []
+			for(var i=0; i<curWidth; i++){
+				r.push({height: newHeight});
+			}
+			return r.concat(generateChunk(curWidth - chunkWidth, newHeight))
+		}
+	}
+
+	return generateChunk(width, initialHeight);
 
 }
